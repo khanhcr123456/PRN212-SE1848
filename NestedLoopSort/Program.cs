c978f6b339ca430af2f8983ff4d76459cc304a74
Huỳnh Đức Khánh <khanhhdse180719@fpt.edu.vn>
2025-05-26 11:53:10 +0700

    Add files via upload

---
+++ NestedLoopSort/Program.cs
@@ -0,0 +1,77 @@
+﻿void swap(ref int x,ref int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+void sort_array(int []arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = i+1;j<arr.Length;j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                swap( ref arr[i], ref arr[j]);
+            }
+        }
+    }
+}
+void sort_array_while(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        int j = i + 1;
+        while (j < arr.Length)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(ref arr[i], ref arr[j]);
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+void sort_array_do_while(int[] arr)
+{
+    int i = 0;
+    do
+    {
+        int j = i + 1;
+        do
+        {
+            if (j < arr.Length && arr[i] > arr[j])
+            {
+                swap(ref arr[i], ref arr[j]);
+            }
+            j++;
+        } while (j < arr.Length);
+        i++;
+    } while (i < arr.Length);
+}
+
+int[] values = new int[5];
+void create_array(int[] values)
+{
+    Random rd = new Random();
+    for (int i = 0; i < values.Length; i++)
+    {
+        values[i] = rd.Next(100);
+    }
+}
+void print_array(int[] values)
+{
+    foreach (int value in values)
+        Console.WriteLine($"{value}\t");
+}
+
+
+
+create_array(values);
+print_array(values);
+sort_array_do_while(values);
+Console.WriteLine("\nAfter sorting:");
+print_array(values);
