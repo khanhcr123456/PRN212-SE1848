e8314963e4b9691b6962cd8886bc2334ce9c479b
Huỳnh Đức Khánh <khanhhdse180719@fpt.edu.vn>
2025-05-26 11:51:24 +0700

    Add files via upload

---
+++ FirstDegree/Program.cs
@@ -0,0 +1,25 @@
+﻿void first_degree_solution(double a, double b)
+{
+    if (a == 0 && b == 0)
+    {
+        //0x+0=0
+        Console.WriteLine("Vo so nghiem");
+    }
+    else if (a == 0 && b != 0)
+    {
+        Console.WriteLine("vo nghiem");
+    }
+    else
+    {
+        Console.WriteLine("X={0}", -b / a);
+    }
+
+}
+Console.WriteLine("Phuong trinh bac 1 :ax+b=0");
+Console.WriteLine("nhap a");
+double a=double.Parse(Console.ReadLine());
+Console.WriteLine("nhap b");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("{0}x+{1}=0", a, b);
+first_degree_solution(a, b);
+Console.ReadLine();
---
+++ FisrtProject/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text;
+
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Xin chaof casc banj");
+Console.WriteLine("toi la se1848");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Toi hua se hoc nghiem tuc");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("toi hua toi se lam");
+Console.ReadLine();
